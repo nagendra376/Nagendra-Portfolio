ee7e54a02e1f15deeb4eb2035d7d82adc6af194b
nodeanurag <conveytoanurag@gmail.com>
2026-06-30 21:51:47 +0530

    add: create page.tsx with component imports for portfolio layout

---
+++ anuragportfolio/app/page.tsx
@@ -0,0 +1,8 @@
+import { Hero } from "@/components/hero";
+import { About } from "@/components/about";
+import { Experience } from "@/components/experience";
+import { Projects } from "@/components/projects";
+import { Skills } from "@/components/skills";
+import { Writing } from "@/components/writing";
+import { GitHubGraph } from "@/components/github-graph";
+import { ScrollProgress } from "@/components/scroll-progress";
