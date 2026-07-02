8a35740bea188de3d47532be2d4b880cd6229c24
nodeanurag <conveytoanurag@gmail.com>
2026-07-02 11:34:23 +0530

    feat: add GitHub contribution graph, Konami code easter egg, and portfolio configuration file

---
+++ anuragportfolio/components/writing.tsx
@@ -0,0 +1,11 @@
+import { site } from "@/config/site";
+import { Reveal, SectionHeader } from "./reveal";
+import { ArrowUpRight } from "./icons";
+
+export function Writing() {
+  if (!site.writing.length) return null;
+
+  return (
+    
+  );
+}
